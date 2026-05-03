b49f0464ab5a9867ea62d8c895cfe75dea554e35
chillcoldwater <ferera1012@gmail.com>
2026-05-03 19:33:16 +0300

    Add asteroids array
    Теперь карточки создаются в соответствии с данными массивов + добавил isDanger для смены цвета карточки

--- asteroids/src/components/asteroids-list/AsteroidsList.jsx
+++ asteroids/src/components/asteroids-list/AsteroidsList.jsx
@@ -4,30 +4,46 @@ import styles from "./AsteroidsList.module.css";
 export const AsteroidsList = () => {
   return (
     <div className={styles.asteroids}>
-      <AsteroidCard
-        name={"арбуз"}
-        distance={5145}
-        diameter={500}
-        date={"30 мая 2020"}
-      />
-      <AsteroidCard
-        name={"дыня"}
-        distance={1000}
-        diameter={100}
-        date={"25 мая 2020"}
-      />
-      <AsteroidCard
-        name={"вкусный фрукт"}
-        distance={3000}
-        diameter={300}
-        date={"24 мая 2020"}
-      />
-      <AsteroidCard
-        name={"яблоко"}
-        distance={70000}
-        diameter={700}
-        date={"23 мая 2020"}
-      />
+      {asteroids.map(({ name, distance, diameter, date, isDanger }) => (
+        <AsteroidCard
+          name={name}
+          distance={distance}
+          diameter={diameter}
+          date={date}
+          isDanger={isDanger}
+        />
+      ))}
     </div>
   );
 };
+
+const asteroids = [
+  {
+    name: "арбуз",
+    distance: 5145,
+    diameter: 500,
+    date: "30 мая 2020",
+    isDanger: false,
+  },
+  {
+    name: "дыня",
+    distance: 1000,
+    diameter: 100,
+    date: "25 мая 2020",
+    isDanger: true,
+  },
+  {
+    name: "вкусный фрукт",
+    distance: 3000,
+    diameter: 300,
+    date: "24 мая 2020",
+    isDanger: false,
+  },
+  {
+    name: "яблоко",
+    distance: 70000,
+    diameter: 700,
+    date: "23 мая 2020",
+    isDanger: false,
+  },
+];
